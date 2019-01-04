5b7e4a0ecf82c973f8314b179065acf304391a04
John Castberg <jpcastberg@gmail.com>
2019-01-04 14:58:33 -0800

    completed functions in functional instantiations

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,27 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var posInQueue = (someInstance.size()).toString();
+    storage[posInQueue] = value;
   };
 
   someInstance.dequeue = function() {
+    var deletedValue = storage['0'];
+    delete storage['0'];
+    for (var i = 0; i < someInstance.size(); i++) {
+      var currentKey = (i + 1).toString();
+      storage[(i).toString()] = storage[currentKey];
+      delete storage[currentKey];
+    }
+    return deletedValue;
   };
 
   someInstance.size = function() {
+    var size = 0;
+    for(x in storage){
+      size++;
+    }
+    return size;
   };
 
   return someInstance;
