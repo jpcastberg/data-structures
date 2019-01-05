e3cabb41be6ccd225c973597ae3cbcbaa0cc2716
John Castberg <jpcastberg@gmail.com>
2019-01-04 17:53:21 -0800

    Finished ES6 instantiation patterns

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,30 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  enqueue(value) {
+    var posInQueue = (this.size()).toString();
+    this.storage[posInQueue] = value;
+  }
+
+  dequeue() {
+    var deletedValue = this.storage['0'];
+    delete this.storage['0'];
+    for (var i = 0; i < this.size(); i++) {
+      var currentKey = (i + 1).toString();
+      this.storage[(i).toString()] = this.storage[currentKey];
+      delete this.storage[currentKey];
+    }
+    return deletedValue;
+  }
+
+  size() {
+    var size = 0;
+    for (var x in this.storage) {
+      size++;
+    }
+    return size;
+  }
 }
