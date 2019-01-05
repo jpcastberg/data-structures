6e87b8dabbfa71c38c76dc81749dc210ef497461
jpcastberg <jpcastberg@gmail.com>
2019-01-05 11:26:28 -0800

    Began psuedocode

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,21 +1,22 @@
-var LinkedList = function() {
+var LinkedList = function () {
   var list = {};
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value) {
+  list.addToTail = function (value) {
+    // If (!list.head && !list.tail) We set list.head AND list.tail to the value
   };
 
-  list.removeHead = function() {
+  list.removeHead = function () {
   };
 
-  list.contains = function(target) {
+  list.contains = function (target) {
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function (value) {
   var node = {};
 
   node.value = value;
