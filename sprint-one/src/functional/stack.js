5e209bf8b7592102c345a6a2c1664610a53d0b05
John Castberg <jpcastberg@gmail.com>
2019-01-04 21:18:09 -0800

    finished functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,25 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //pass in value
+    //assign value as key in storage, one higher thst the last one
+    var key = someInstance.size().toString();
+    storage[key] = value;
   };
 
   someInstance.pop = function() {
+    var key = (someInstance.size() - 1).toString();
+    var popped = storage[key];
+    delete storage[key];
+    return popped;
   };
 
   someInstance.size = function() {
+    var stackSize = 0;
+    for (var x in storage) {
+      stackSize++;
+    }
+    return stackSize;
   };
 
   return someInstance;
