874a4923c90fca01d7bab473294162dda81a45fc
jpcastberg <jpcastberg@gmail.com>
2019-01-07 17:50:46 -0800

    Got tree structure working

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,21 +1,51 @@
+
+// Write an extend function to use for the class Tree
+
+var extend = function(obj, methods) {
+  for (var prop in methods) {
+    obj[prop] = methods[prop];
+  }
+
+}
+
 var Tree = function (value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  //add methods form treeMethods (for in)
   newTree.children = [];  // fix me
-
+  
+  extend(newTree, treeMethods)
+  // extend newTree with the methods below
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function (value) {
-  //
+  this.children.push(Tree(value));
+  // Pushing new tree into its parents.children array 
 };
 
 treeMethods.contains = function (target) {
+  // Recursively traverse Tree and if any child's value matches the target, return true
+  //define inner function that takes in an obj
+  // if obj.value === target, return true
+  //else call recursive function on each child node using for loop
+  var treeContains = false;
+
+  var nodeContains = function (node) {
+    if (node.value === target) {
+      treeContains = true;
+      return;
+    } else {
+      for (var i = 0; i < node.children.length; i++) {
+        if (!treeContains) {
+          nodeContains(node.children[i]);
+        }
+      }
+    }
+  }
+  nodeContains(this)
+  return treeContains;
 };
 
 
@@ -23,3 +53,5 @@ treeMethods.contains = function (target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+//var topOfTree = Tree();
